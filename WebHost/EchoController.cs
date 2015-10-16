3810e09332543a759208d7e505d56ebcce8a8706
Pietro Martinelli <amicofragile@gmail.com>
2015-10-16 22:10:33 +0200

    WIREUP Simple Self hosted webapp, Owin based

---
+++ WebHost/EchoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+namespace WebHost
+{
+    [RoutePrefix("/echo")]
+    public class EchoController: ApiController
+    {
+        [Route()]
+        public EchoClientMessage GetEcho(string target)
+        {
+            return new EchoClientMessage()
+            {
+                to = target,
+                message = string.Format("Hello, {0}!", target)
+            };
+        }
+
+        public class EchoClientMessage
+        {
+            public string to { get; set; }
+            public string message { get; set; }
+        }
+    }
+}
---
+++ WebHost/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace WebHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+            WebApp.Start<Startup>(url: baseAddress);
+            /*
+
+            // Start OWIN host 
+            using ()
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/echo?target=World").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            */
+            Console.ReadLine();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
